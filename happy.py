9c25ebdd0840390769abe8b162609c2c2500c3a4
Barret Rennie <barret.rennie@gmail.com>
2014-05-01 13:51:23 -0600

    PEP8

--- happy.py
+++ happy.py
@@ -5,79 +5,79 @@
 import sys
 
 def happificate(n):
-	sum = 0
-	while n > 0:
-		digit = n % 10
-		n //= 10
-		sum += digit ** 2
+    sum = 0
+    while n > 0:
+        digit = n % 10
+        n //= 10
+        sum += digit ** 2
 
-	return sum
+    return sum
 
 def happificateRange(begin, end):
-	graph = {}
+    graph = {}
 
-	for n in range(begin, end + 1):
-		if n not in graph:
-			result = happificate(n)
-			graph[n] = result
+    for n in range(begin, end + 1):
+        if n not in graph:
+            result = happificate(n)
+            graph[n] = result
 
-			while result > end and result not in graph:
-				n = result
-				result = happificate(n)
-				graph[n] = result
+            while result > end and result not in graph:
+                n = result
+                result = happificate(n)
+                graph[n] = result
 
-	return graph
+    return graph
 
 def seperate(graph):
-	happy = []
-	unhappy = []
+    happy = []
+    unhappy = []
 
-	for k in graph:
-		next = graph[k]
-		path = [k]
+    for k in graph:
+        next = graph[k]
+        path = [k]
 
-		while next != 1 and next not in path:
-			path.append(next)
-			next = graph[next]
+        while next != 1 and next not in path:
+            path.append(next)
+            next = graph[next]
 
-		if next == 1:
-			happy.append(k)
-		else:
-			unhappy.append(k)
+        if next == 1:
+            happy.append(k)
+        else:
+            unhappy.append(k)
 
-	return (happy, unhappy)
+    return (happy, unhappy)
 
 
 
 def graphToDot(graph):
 
-	happy,unhappy = seperate(graph)
+    happy, unhappy = seperate(graph)
 
-	print("digraph {")
+    print("digraph {")
 
-	print("\tsubgraph cluster_happy {")
-	print("\t\tgraph [label=\"Happy Numbers\", style=dotted];")
+    print("\tsubgraph cluster_happy {")
+    print("\t\tgraph [label=\"Happy Numbers\", style=dotted];")
 
 
-	for k in happy:
-		print("\t\tn{0} [label=\"{0}\"];".format(k))
-		print("\t\tn{0} -> n{1};".format(k, graph[k]))
+    for k in happy:
+        print("\t\tn{0} [label=\"{0}\"];".format(k))
+        print("\t\tn{0} -> n{1};".format(k, graph[k]))
 
-	print("\t}")
+    print("\t}")
 
-	print("\tsubgraph cluster_melancoil {")
-	print("\t\tgraph [label=\"Melancoil\", style=dotted];")
+    print("\tsubgraph cluster_melancoil {")
+    print("\t\tgraph [label=\"Melancoil\", style=dotted];")
 
-	for k in unhappy:
-		print("\t\tn{0} [label=\"{0}\"];".format(k))
-		print("\t\tn{0} -> n{1};".format(k, graph[k]))
-	print("\t}")
+    for k in unhappy:
+        print("\t\tn{0} [label=\"{0}\"];".format(k))
+        print("\t\tn{0} -> n{1};".format(k, graph[k]))
+    print("\t}")
 
-	print("}")
+    print("}")
 
 if __name__ == '__main__':
-	if len(sys.argv) != 2:
-		print("usage: {0} MAXIMUM".format(sys.argv[0]))
-		print("Create a graph of the happy numbers from 1 to MAXIMUM")
-	
-	graphToDot(happificateRange(1, int(sys.argv[1])))
+    if len(sys.argv) != 2:
+        print("usage: {0} MAXIMUM".format(sys.argv[0]))
+        print("Create a graph of the happy numbers from 1 to MAXIMUM")
+    
+    graphToDot(happificateRange(1, int(sys.argv[1])))
